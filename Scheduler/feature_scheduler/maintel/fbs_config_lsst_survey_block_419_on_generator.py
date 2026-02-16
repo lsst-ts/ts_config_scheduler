5f5c879f1450ae36a496633fc2ff1982e6a83cb6
Lynne Jones <ljones.uw@gmail.com>
2026-02-16 12:02:07 -0800

    Update hashes for 419 surveys

--- Scheduler/feature_scheduler/maintel/fbs_config_lsst_survey_block_419_on_generator.py
+++ Scheduler/feature_scheduler/maintel/fbs_config_lsst_survey_block_419_on_generator.py
@@ -192,7 +192,7 @@ def get_scheduler() -> tuple[int, CoreScheduler]:
     # This hash is provided by the script that
     # generates the pre-computed data. Execute it and paste
     # the provided value here.
-    expected_hex_digest = "86d6f33"
+    expected_hex_digest = "c9392ba"
     pre_comp_file = (
         pathlib.Path(get_data_dir())
         / "scheduler"
