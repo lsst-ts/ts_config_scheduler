9640dfe6908c328bd1ab9f4a60e1495af4a299a2
Peter Yoachim <yoachim@uw.edu>
2026-09-22 13:30:50 -0700

    update template survey config

--- Scheduler/feature_scheduler/maintel/fbs_config_lsst_survey.py
+++ Scheduler/feature_scheduler/maintel/fbs_config_lsst_survey.py
@@ -299,31 +299,16 @@ def get_scheduler(for_simulation=False) -> tuple[int, CoreScheduler]:
     template_mask_params = copy.deepcopy(standard_mask_params)
     template_mask_params["max_alt"] = min(blob_max_alt, standard_mask_params["max_alt"])
 
+    band1s = ["u", "g", "g", "r", "r", "i", "r", "z", "y"]
+    band2s = ["u", "g", "r", "r", "i", "z", "z", "y", "y"]
+
+    # In year 1, only use the default large area
+    # limit. 
     template_surveys_1 = lsst_surveys.gen_template_surveys(
         template_fp,
         nside=nside,
-        band1s=[
-            "u",
-            "g",
-            "g",
-            "r",
-            "r",
-            "i",
-            "r",
-            "z",
-            "y",
-        ],
-        band2s=[
-            "u",
-            "g",
-            "r",
-            "r",
-            "i",
-            "z",
-            "z",
-            "y",
-            "y",
-        ],
+        band1s=band1s,
+        band2s=band2s,
         seeing_fwhm_max_zenith=fwhm_template_max_zenith,
         median_cloud_limit=2.0,
         extinction_limit=1.0,
@@ -333,35 +318,21 @@ def get_scheduler(for_simulation=False) -> tuple[int, CoreScheduler]:
         n_obs_template={"u": 3, "g": 3, "r": 3, "i": 3, "z": 3, "y": 3},
         night_min=0,
         night_max=365,
+        additional_area_limits=(),
+        extra_HA_mins=(),
+        extra_HA_maxes=(),
         science_program=science_program,
         blob_survey_params=blob_survey_params,
         standard_mask_params=template_mask_params,
     )
+
+    # In year 2, use the additional limits to fill small 
+    # islands that have not yet reached 6 template images
     template_surveys_2 = lsst_surveys.gen_template_surveys(
         template_fp,
         nside=nside,
-        band1s=[
-            "u",
-            "g",
-            "g",
-            "r",
-            "r",
-            "i",
-            "r",
-            "z",
-            "y",
-        ],
-        band2s=[
-            "u",
-            "g",
-            "r",
-            "r",
-            "i",
-            "z",
-            "z",
-            "y",
-            "y",
-        ],
+        band1s=band1s,
+        band2s=band2s,
         seeing_fwhm_max_zenith=fwhm_template_max_zenith,
         median_cloud_limit=2.0,
         extinction_limit=1.0,
