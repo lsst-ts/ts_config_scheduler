d8a9941fe4e9fdacad6be731623c2a560af2281d
Lynne Jones <ljones.uw@gmail.com>
2026-09-23 14:12:16 -0700

    Update ddf hash; fix matching on_generator version

--- Scheduler/feature_scheduler/maintel/fbs_config_lsst_survey.py
+++ Scheduler/feature_scheduler/maintel/fbs_config_lsst_survey.py
@@ -219,7 +219,7 @@ def get_scheduler() -> tuple[int, CoreScheduler]:
     # This hash is provided by the script that
     # generates the pre-computed data. Execute it and paste
     # the provided value here.
-    expected_hex_digest = "d434bca"
+    expected_hex_digest = "39563ad"
     pre_comp_file = (
         pathlib.Path(get_data_dir())
         / "scheduler"
